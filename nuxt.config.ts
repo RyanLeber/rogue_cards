f9dbb7fa23495e20b551bb4af76bb192c6447470
RyanLeber <ryanleber1@gmail.com>
2024-02-22 14:36:25 -0500

    added new font to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        {rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"}
+        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"},
+        { rel:"preconnect", href: "https://fonts.googleapis.com"},
+        { rel:"preconnect", href: "https://fonts.gstatic.com"},
+        { href:"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap", rel:"stylesheet"},
       ]
     }
   }
