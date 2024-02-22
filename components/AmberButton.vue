2c097472746b77f58b0c977a87251f3c25f73557
RyanLeber <ryanleber1@gmail.com>
2024-02-22 14:38:14 -0500

    created a default button

---
+++ components/AmberButton.vue
@@ -0,0 +1,23 @@
+ 
+<template>
+  <button :type="type | 'button'"
+    class="
+      flex w-fit justify-center rounded-md
+      px-3 py-1.5 text-sm font-semibold 
+      leading-6 text-white shadow-sm 
+      focus-visible:outline 
+      bg-amber-600 
+      outline-amber-500 
+      hover:bg-amber-500
+    "
+  > 
+    <slot /> 
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  type: String
+})
+
+</script>
