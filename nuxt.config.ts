69f8d58adce842a867e336dcabfda5cca2618311
RyanLeber <ryanleber1@gmail.com>
2024-02-24 14:12:40 -0500

    added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,7 @@ export default defineNuxtConfig({
         { href:"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap", rel:"stylesheet"},
       ]
     }
-  }
+  },
+
+  modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt" ]
 })
