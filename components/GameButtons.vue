df3385e0e27b5b7a72d1719cd62f78549c5edd4e
RyanLeber <ryanleber1@gmail.com>
2024-02-26 12:49:59 -0500

    created button group for game control

---
+++ components/GameButtons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="
+    flex lg:flex-row flex-col items-center justify-center
+    w-fit h-fit
+    my-8 p-4
+    border-slate-500 rounded-lg border-2
+    bg-slate-300"
+  >
+
+  <AmberButton class="lg:mx-4 my-2">End Turn</AmberButton>
+  <AmberButton class="lg:mx-4 my-2">Battle Lost</AmberButton>
+  <AmberButton class="lg:mx-4 my-2">Battle Won</AmberButton>
+
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
